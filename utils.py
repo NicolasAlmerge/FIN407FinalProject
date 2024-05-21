a81e383bb08771b4e0b8ba502227667b9a1915ff
Nicolas Almerge <nicolas.almerge@gmail.com>
2024-05-21 16:10:19 +0200

    Added predictors

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
+import logging
 import os
+from rich.logging import RichHandler
 from typing import Literal
 
 
 __all__ = (
     "get_data_file_path",
     "get_env",
+    "get_logger",
 )
 
 
@@ -19,3 +22,17 @@ def get_env(key: Literal["WRDS_USERNAME"], /) -> str:
             if line_key.strip() == key:
                 return line_value.strip()
     raise ValueError("Could not find requested key")
+
+
+def get_logger(name: str, minimum_level: int = logging.DEBUG) -> logging.Logger:
+    """
+    Gets a custom logger.
+
+    :param name: Name of the logger.
+    :param minimum_level: Minimum level of the logger.
+    :return: Custom-made logger.
+    """
+    logging.basicConfig(format="%(message)s", datefmt="[%X]", handlers=[RichHandler()])
+    logger = logging.getLogger(name)
+    logger.setLevel(minimum_level)
+    return logger
